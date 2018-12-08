b787c33876e4ae739d799c977daaf082b7ad6b62
Guerrat <salimi.mehdy@gmail.com>
2018-12-08 17:36:29 +0100

    Maj de l'architecture

--- base.sql
+++ base.sql
@@ -15,7 +15,7 @@ drop table COTISER cascade constraints;
 drop table ASSOCIATIONS cascade constraints;
 drop table NEWS cascade constraints;
 drop table RESPONSABLES cascade constraints;
-drop table GERER cascade constraints;
+drop table ORGANISER cascade constraints;
 
 drop sequence ID_PERS_SEQ;
 drop sequence ID_EVENT_SEQ;
@@ -25,7 +25,7 @@ drop sequence ID_FIN_SEQ;
 -- ============================================================
 create table PERSONNES
 (
-    ID_PERS             NUMBER(5)       not null ,
+    ID_PERS             NUMBER(5)           not null                ,
     NOM_PERS            VARCHAR2(40)        not null                ,
     PRENOM_PERS         VARCHAR2(40)        not null                ,
     constraint pk_pers primary key (ID_PERS)
@@ -33,34 +33,33 @@ create table PERSONNES
 
 create table PARTICIPER
 (
-    ID_EVENT            NUMBER(5)       not null                ,
-    ID_PERS             NUMBER(5)       not null                ,
-    TARIF               NUMBER(5)       DEFAULT 0               ,
+    ID_EVENT            NUMBER(5)           not null                ,
+    ID_PERS             NUMBER(5)           not null                ,
     COMMENTAIRE_EVENT   VARCHAR2(280)                               ,
-    NOTE                NUMBER(1)                               ,
+    NOTE                NUMBER(1)                                   ,
     constraint pk_part primary key (ID_EVENT, ID_PERS)
 );
 
 create table EVENEMENTS
 (
-    ID_EVENT            NUMBER(5)       not null ,
+    ID_EVENT            NUMBER(5)           not null                ,
     TITRE_EVENT         VARCHAR2(100)       not null                ,
     DESCRIPTION         VARCHAR2(1000)      not null                ,
-    DATE_CREATION       DATE            not null                ,
-    COUT                NUMBER(6)       DEFAULT 0               ,
+    DATE_CREATION       DATE                not null                ,
+    COUT                NUMBER(6)           DEFAULT 0               ,
     LIEU                VARCHAR2(1000)      not null                ,
-    NB_PLACE            NUMBER(6)       DEFAULT 0               ,
-    DATE_DEBUT          DATE            not null                ,
-    DATE_FIN            DATE            not null                ,
+    TARIF_ADHERENT      NUMBER(3)           DEFAULT 0               ,
+    TARIF_EXTE          NUMBER(3)           DEFAULT 0               ,
+    NB_PLACE            NUMBER(6)           DEFAULT 0               ,
+    DATE_DEBUT          DATE                not null                ,
+    DATE_FIN            DATE                not null                ,
     FREQUENCE           VARCHAR2(12) check (FREQUENCE in ('QUOTIDIEN', 'HEBDOMADAIRE', 'MENSUEL', 'ANNUEL')),
-    NOM_ASSO            VARCHAR2(40)        not null                ,
-    LOGIN_ORGANISATEUR  VARCHAR2(40)        not null                ,
     constraint pk_event primary key (ID_EVENT)
 );
 
 create table SOURCES_FINANCEMENT
 (
-    ID_FIN              NUMBER(5)       not null ,
+    ID_FIN              NUMBER(5)           not null                ,
     TYPE                VARCHAR2(10) check (type in ('ENTREPRISE', 'PRIVEE', 'ETAT', 'ECOLE')) not null,
     NOM_FIN             VARCHAR2(100)       DEFAULT 'Anonyme'       ,
     constraint pk_sfin primary key (ID_FIN)
@@ -79,60 +78,63 @@ create table ADHERENTS
 
 create table FINANCER
 (
-    ID_FIN              NUMBER(5)       not null                ,
+    ID_FIN              NUMBER(5)           not null                ,
     NOM_ASSO            VARCHAR2(40)        not null                ,
-    SOMME               NUMBER(6)       DEFAULT 0               ,
+    SOMME               NUMBER(6)           DEFAULT 0               ,
     constraint pk_fin primary key (ID_FIN, NOM_ASSO)
 );
 
 create table COMMENTER
 (
-  ID_NEWS               NUMBER(5)       not null                ,
-  LOGIN_ADHERENT        VARCHAR2(40)        not null                ,
-  COMMENTAIRE_NEWS      VARCHAR2(280)                               ,
-  constraint pk_com primary key (ID_NEWS)
+    ID_NEWS               NUMBER(5)         not null                ,
+    LOGIN_ADHERENT        VARCHAR2(40)      not null                ,
+    COMMENTAIRE_NEWS      VARCHAR2(280)                             ,
+    constraint pk_com primary key (ID_NEWS)
 );
 
 create table COTISER
 (
-  LOGIN_ADHERENT        VARCHAR2(40)        not null                ,
-  NOM_ASSO              VARCHAR2(40)        not null                ,
-  DATE_INSCRIPTION      DATE            not null                ,
-  constraint pk_cot primary key (LOGIN_ADHERENT, NOM_ASSO)
+    LOGIN_ADHERENT        VARCHAR2(40)      not null                ,
+    NOM_ASSO              VARCHAR2(40)      not null                ,
+    DATE_INSCRIPTION      DATE              not null                ,
+    constraint pk_cot primary key (LOGIN_ADHERENT, NOM_ASSO)
 );
 
 create table ASSOCIATIONS
 (
-  NOM_ASSO              VARCHAR2(40)       not null                ,
-  OBJECTIF              VARCHAR2(280)      not null                ,
-  MONTANT_COTISATION    NUMBER(6)      default 0               ,
-  constraint pk_asso primary key (NOM_ASSO)
+    NOM_ASSO              VARCHAR2(40)       not null                ,
+    OBJECTIF              VARCHAR2(280)      not null                ,
+    MONTANT_COTISATION    NUMBER(6)          DEFAULT 0               ,
+    constraint pk_asso primary key (NOM_ASSO)
 );
 
 create table NEWS
 (
-  ID_NEWS               NUMBER(5)      not null               ,
-  TITRE_NEWS            VARCHAR2(40)       not null               ,
-  TEXTE                 VARCHAR2(1000)     not null               ,
-  DATE_CREATION         DATE           not null               ,
-  NOM_ASSO              VARCHAR2(40)       not null               ,
-  constraint pk_news primary key (ID_NEWS)
+    ID_NEWS               NUMBER(5)          not null               ,
+    TITRE_NEWS            VARCHAR2(40)       not null               ,
+    TEXTE                 VARCHAR2(1000)     not null               ,
+    DATE_CREATION         DATE               not null               ,
+    ID_RESPO              NUMBER(5)          not null               ,
+    constraint pk_news primary key (ID_NEWS)
 );
 
 create table RESPONSABLES
 (
-  NOM_ASSO              VARCHAR2(40)      not null                ,
-  FONCTION              VARCHAR2(60)      not null                ,
-  LOGIN_ADHERENT        VARCHAR2(40)      not null                ,
-  constraint pk_respo primary key (NOM_ASSO, FONCTION)
+    ID_RESPO              NUMBER(5)         not null                ,
+    FONCTION              VARCHAR2(60)      not null                ,
+    LOGIN_ADHERENT        VARCHAR2(40)      not null                ,
+    NOM_ASSO              VARCHAR2(40)      not null                ,
+    constraint pk_respo primary key (ID_RESPO)
 );
 
-create table GERER
+create table ORGANISER
 (
-  NOM_ASSO              VARCHAR2(40)      not null                ,
-  FONCTION              VARCHAR2(60)      not null                ,
-  constraint pk_ger primary key (NOM_ASSO, FONCTION)
+    ID_EVENT              NUMBER(5)         not null                ,
+    NOM_ASSO              VARCHAR2(40)      not null                ,
+    LOGIN_ADHERENT        VARCHAR2(40)      not null                ,
+    constraint pk_orga primary key (ID_EVENT, NOM_ASSO, LOGIN_ADHERENT)
 );
+
 
 
 create sequence ID_PERS_SEQ
@@ -158,6 +160,13 @@ create sequence ID_FIN_SEQ
 
 
 create sequence ID_NEWS_SEQ
+  minvalue 1
+  maxvalue 99999
+  start with 1
+  increment by 1
+  cache 20;
+
+create sequence ID_FIN_SEQ
   minvalue 1
   maxvalue 99999
   start with 1
--- donnees.sql
+++ donnees.sql
@@ -13,7 +13,7 @@ delete from COTISER ;
 delete from ASSOCIATIONS ;
 delete from NEWS ;
 delete from RESPONSABLES ;
-delete from GERER ;
+delete from ORGANISER ;
 
 commit ;
 
@@ -23,40 +23,40 @@ commit ;
 
 -- PERSONNES
 
-insert into PERSONNES (ID_PERS_SEQ, 'BURÉ', 'JULIEN');
-insert into PERSONNES (ID_PERS_SEQ, 'GAUDRÉ', 'GABIN');
-insert into PERSONNES (ID_PERS_SEQ, 'SALIMI', 'MEHDY');
-insert into PERSONNES (ID_PERS_SEQ, 'SEDON', 'PIERRE');
-insert into PERSONNES (ID_PERS_SEQ, 'MARTINEZ', 'AXEL');
-insert into PERSONNES (ID_PERS_SEQ, 'AGUILERA', 'AYMERIC');
-insert into PERSONNES (ID_PERS_SEQ, 'PERIS', 'SARAH');
-insert into PERSONNES (ID_PERS_SEQ, 'SONET', 'LUCA');
-insert into PERSONNES (ID_PERS_SEQ, 'LEE SHAO SHIT', 'TÉRENCE');
-insert into PERSONNES (ID_PERS_SEQ, 'ROYANT', 'YANN');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'BURÉ', 'JULIEN');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'GAUDRÉ', 'GABIN');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'SALIMI', 'MEHDY');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'SEDON', 'PIERRE');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'MARTINEZ', 'AXEL');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'AGUILERA', 'AYMERIC');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'PERIS', 'SARAH');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'SONET', 'LUCA');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'LEE SHAO SHIT', 'TÉRENCE');
+insert into PERSONNES (ID_PERS_SEQ.nextval, 'ROYANT', 'YANN');
 
 commit;
 
 
 -- PARTICIPER
 
-insert into PARTICIPER (2, 1, 2, 'J\'en ai pris plein la vue, merci Jacquie et Mich\'eirb', 9);
-insert into PARTICIPER (1, 8, 8, 'Le prix ça fait mal au cul !!', 2);
+insert into PARTICIPER (2, 1, 2, 'J''en ai pris plein la vue, merci Jacquie et Mich''eirb', 9);
+insert into PARTICIPER (1, 8, 8, 'Le prix ca fait mal au cul !!', 2);
 
 commit;
 
 -- EVENEMENTS
 
-insert into EVENEMENTS (ID_EVENT_SEQ, 'GALA', 'GALA organisé pour célébrer le diplôme des 4A', 24-NOV-18, 55000, 'Chateau Lafitte', 500, 24-NOV-18, 24-NOV-18, NULL, 'GALA', cdelignac);
-insert into EVENEMENTS (ID_EVENT_SEQ, 'BARTOUTATISE', 'Soirée organisée par la Cafétéria de l\'ENSEIRB-MATMECA', 7-NOV-18, 100, 'Cafétéria de l\'école', 400, 7-NOV-18, 7-NOV-18, NULL, 'Le BAR de l\'ENSEIRB-MATMECA', aacosta);
+insert into EVENEMENTS (ID_EVENT_SEQ.nextval, 'GALA', 'GALA organisé pour célébrer le diplôme des 4A', 24-NOV-18, 55000, 'Chateau Lafitte', 15, 15, 500, 24-NOV-18, 24-NOV-18, NULL, 'GALA', cdelignac);
+insert into EVENEMENTS (ID_EVENT_SEQ.nextval, 'BARTOUTATISE', 'Soirée organisée par la Cafétéria de l''ENSEIRB-MATMECA', 7-NOV-18, 100, 'Cafétéria de l''école', 0, 0, 400, 7-NOV-18, 7-NOV-18, NULL, 'Le BAR de l''ENSEIRB-MATMECA', aacosta);
 
 commit;
 
 
 -- SOURCES_FINANCEMENT
 
-insert into SOURCES_FINANCEMENT(ID_FIN_SEQ, 'ENTREPRISE', 'THALES');
-insert into SOURCES_FINANCEMENT(ID_FIN_SEQ, 'PRIVEE', 'DUVIVIER');
-insert into SOURCES_FINANCEMENT(ID_FIN_SEQ, 'ETAT', 'MINISTÈRE DE LA RECHERCHE ET DE L\'ENSEIGNEMENT SUPÉRIEUR');
+insert into SOURCES_FINANCEMENT(ID_FIN_SEQ.nextval, 'ENTREPRISE', 'THALES');
+insert into SOURCES_FINANCEMENT(ID_FIN_SEQ.nextval, 'PRIVEE', 'DUVIVIER');
+insert into SOURCES_FINANCEMENT(ID_FIN_SEQ.nextval, 'ETAT', 'MINISTÈRE DE LA RECHERCHE ET DE L''ENSEIGNEMENT SUPÉRIEUR');
 
 commit;
 
@@ -79,10 +79,10 @@ commit;
 
 -- COMMENTER
 
-insert into COMMENTER values (1 , 'jbure' , 'Moi aussi j\'adore les kitkats ! J\'adore aussi prendre un petit minute maid avant la sodo '    );
+insert into COMMENTER values (1 , 'jbure' , 'Moi aussi j''adore les kitkats ! J''adore aussi prendre un petit minute maid avant la sodo '    );
 insert into COMMENTER values (2 , 'msalimi' , 'Wahou !' );
-insert into COMMENTER values (2 , 'ecollignon' , ' Les Bonjour, la signature et les PS c\'est pour les chiens?');
-insert into COMMENTER values (3 , 'abrama' , 'Je tiens à préciser que ce genre de choses ne m\'arrive que quand je fais des crises de somnanbulisme donc faites pas les bâtards');
+insert into COMMENTER values (2 , 'ecollignon' , ' Les Bonjour, la signature et les PS c''est pour les chiens?');
+insert into COMMENTER values (3 , 'abrama' , 'Je tiens à préciser que ce genre de choses ne m''arrive que quand je fais des crises de somnanbulisme donc faites pas les bâtards');
 insert into COMMENTER values (4 , 'arolin' , 'On dirait moi avec Thierry Bécaro mdr ^^ ' );
 
 commit;
@@ -92,45 +92,42 @@ commit;
 insert into COTISER values ( 'msamili' , 'EIRBWARE', 12-NOV-17 );
 insert into COTISER values ( 'jbure' , 'EMK' , 15-OCT-17 );
 insert into COTISER values ('ggaudre' , 'BDS' , 6-SEP-17 );
-insert into COTISER values ( 'psedon' , 'Ligue des maitres de l\'espace et du temps' , 15-DEC-50);
+insert into COTISER values ( 'psedon' , 'Ligue des maitres de l''espace et du temps' , 15-DEC-50);
 
 commit;
 
 --ASSOCIATIONS
 
-insert into ASSOCIATIONS values ('Ligue des maitres de l\'espace et du temps', 'Dominer le système solaire par la force de la connaissance' , '999999');
-insert into ASSOCIATIONS values ( 'Le BAR de l\'ENSEIRB-MATMECA' , ' Être l\'asso la plus fraiche de cette putain d\'école' , );
+insert into ASSOCIATIONS values ('Ligue des maitres de l''espace et du temps', 'Dominer le système solaire par la force de la connaissance' , '999999');
+insert into ASSOCIATIONS values ( 'Le BAR de l''ENSEIRB-MATMECA' , ' Être l''asso la plus fraiche de cette putain d''école' , );
 insert into ASSOCIATIONS values ( 'EIRBWARE' , 'Manger des bites enfermé dans un local entre geeks' , );
 insert into ASSOCIATIONS values ( 'INGENIB' , 'Brasser un max de thunes pour tout claquer dans le trou du BAR' , 0);
-insert into ASSOCIATIONS values ( 'PIX\'EIRB' , 'Prendre des photos dossiers' , 5 );
+insert into ASSOCIATIONS values ( 'PIX''EIRB' , 'Prendre des photos dossiers' , 5 );
 
 commit;
 
 --NEWS
 
-insert into NEWS values (ID_NEWS_SEQ , 'Un excellent gouter !', 'Ce vendredi 23 novembre au Bar de l\'ENSEIRB-MATMECA, j\'ai dégusté un bon gouter composé d\'un adorable kitkat acommpagné d\'un délicieux café noir' , 23-NOV-18 , 'Le BAR de l\'ENSEIRB-MATMECA'  );
-insert into NEWS values (ID_NEWS_SEQ , 'P Sedon dans le futur', 'JOURNAL DE BORD : Je suis le célèbre P Sedon, aujourd\'hui je ne saurais donner la date, en effet je vous écris cette missive depuis le futur, je suis parvenu à me transporter temporellement jusqu\'au dimanche de cette semaine alors que nous étions le 23, malheureusement j\' ai par conséquent raté le GALA, je viens donc de perdre quinze balles pour rien, je dois avouer être dégouté, personne ne me crois et tout le monde est persuadé que je suis juste en redescente....'  , 25-NOV-18 , 'EIRBWARE'  );
-insert into NEWS values (ID_NEWS_SEQ , 'Incroyable : Lola perd ses dents en pleine représentation ! ', 'Journaliste : Bonjour Lola que s\'est-il passé?    Lola : Je ne chais pas cheu qu\'il ch\'est paché, j\'ai du glisser sur la flaque de piche d\'Archur, il a du vouloir éviter chon P Ché ! ' , 24-OCT-18 , 'EMK'  );
-insert into NEWS values (ID_NEWS_SEQ , 'Ode au mec qui passe la cireuse', ' Ô toi l\'homme le plus brillant de l\'école, toi qui brille et rend brillant, toi qui lutte contre l\'inertie implacable de la rotation des disques de feutre, toi qui porte en rebelle ton polo trop large, comme un OG digne du prince Xzibit. Tu rends nos vie plus groovy en nous laissant libres, glissant sur la brillance de nos beaux parquets. PEACE  ' , 4-JUL-14 , 'Bruno'  );
+insert into NEWS values (ID_NEWS_SEQ.nextval , 'Un excellent gouter !', 'Ce vendredi 23 novembre au Bar de l''ENSEIRB-MATMECA, j''ai dégusté un bon gouter composé d''un adorable kitkat acommpagné d''un délicieux café noir' , 23-NOV-18 , 'Le BAR de l''ENSEIRB-MATMECA'  );
+insert into NEWS values (ID_NEWS_SEQ.nextval , 'P Sedon dans le futur', 'JOURNAL DE BORD : Je suis le célèbre P Sedon, aujourd''hui je ne saurais donner la date, en effet je vous écris cette missive depuis le futur, je suis parvenu à me transporter temporellement jusqu''au dimanche de cette semaine alors que nous étions le 23, malheureusement j'' ai par conséquent raté le GALA, je viens donc de perdre quinze balles pour rien, je dois avouer être dégouté, personne ne me crois et tout le monde est persuadé que je suis juste en redescente....'  , 25-NOV-18 , 'EIRBWARE'  );
+insert into NEWS values (ID_NEWS_SEQ.nextval , 'Incroyable : Lola perd ses dents en pleine représentation ! ', 'Journaliste : Bonjour Lola que s''est-il passé?    Lola : Je ne chais pas cheu qu''il ch''est paché, j''ai du glisser sur la flaque de piche d''Archur, il a du vouloir éviter chon P Ché ! ' , 24-OCT-18 , 'EMK'  );
+insert into NEWS values (ID_NEWS_SEQ.nextval , 'Ode au mec qui passe la cireuse', ' Ô toi l''homme le plus brillant de l''école, toi qui brille et rend brillant, toi qui lutte contre l''inertie implacable de la rotation des disques de feutre, toi qui porte en rebelle ton polo trop large, comme un OG digne du prince Xzibit. Tu rends nos vie plus groovy en nous laissant libres, glissant sur la brillance de nos beaux parquets. PEACE  ' , 4-JUL-14 , 'Bruno'  );
 
 commit;
 
 --RESPONSABLES
 
-insert into RESPONSABLES values ('Ligue des maitres de l\'espace et du temps' , 'Gourou suprême' , 'psedon');
-insert into RESPONSABLES values ('Le BAR de l\'ENSEIRB-MATMECA' , 'Biero ASKIP' , 'gfontaine');
-insert into RESPONSABLES values ('Chibr\'eirb' , 'Respo con' , 'lsonet' );
-insert into RESPONSABLES values ('BDS' , 'Respo Sécu' , 'helmalaki');
+insert into RESPONSABLES values (ID_RESPO_SEQ.nextval, 'Gourou suprême' , 'psedon', 'Ligue des maitres de l''espace et du temps');
+insert into RESPONSABLES values (ID_RESPO_SEQ.nextval, 'Biero ASKIP' , 'gfontaine', 'Le BAR de l''ENSEIRB-MATMECA');
+insert into RESPONSABLES values (ID_RESPO_SEQ.nextval, 'Respo con' , 'lsonet', 'Chibr''eirb');
+insert into RESPONSABLES values (ID_RESPO_SEQ.nextval, 'Respo Sécu' , 'helmalaki', 'BDS');
 
 commit;
 
---GERER
+--ORGANISER
 
-insert into GERER values ('Le BAR de l\'ENSEIRB-MATMECA' , 'Président');
-insert into GERER values ('BDE' , 'Président');
-insert into GERER values ('Le BAR de l\'ENSEIRB-MATMECA' , 'Respo Soirée');
-insert into GERER values ('Solid\'eirb', 'Trésorier');
-insert into GERER values ('Le BAR de l\'ENSEIRB-MATMECA' , 'Biero Askip');
-insert into GERER values ('BDE' , 'Vice-président');
+insert into ORGANISER values (2, 'Le BAR de l''ENSEIRB-MATMECA', 'ggaudre');
+insert into ORGANISER values (2, 'Le BAR de l''ENSEIRB-MATMECA', 'jbure');
+insert into ORGANISER values (1, 'GALA', 'msalimi');
 
 commit;
