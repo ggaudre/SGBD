d8d8e4470a809822a8205fe6615da94342523e70
BURE Julien <jbure@amoretti.pedago.ipb.fr>
2018-12-08 13:39:16 +0100

    oui

--- requetes_selection.sql
+++ requetes_selection.sql
@@ -1,7 +1,28 @@
 --Informations sur les associations, les adhérents, les évènements.
--- La liste des adhérents à une association à une date donnée et celle
---  des adhérents qui n'ont pas encore cotisé. La liste des personnes organisant un évènement et celles des personnes participant à cet évènement.
---   Pour une news, on veut savoir le nombre de commentaires qui ont été postés.
 
-SELECT *
-FROM ASSOCIATIONS
+
+
+
+
+--La liste des adhérents à une association à une date donnée
+SELECT LOGIN_ADHERENT
+FROM COTISER
+WHERE NOM_ASSO= "BDE" AND DATE_INSCRIPTION = 12-NOV-17 ;
+-- et celle des adhérents qui n'ont pas encore cotisé.
+SELECT LOGIN_ADHERENT
+FROM COTISER
+WHERE NOM_ASSO= "BDE" AND DATE_INSCRIPTION = NULL ;
+
+-- La liste des personnes organisant un évènement
+SELECT LOGIN_ORGANISATEUR
+FROM EVENEMENT
+WHERE ID_EVENT = 1;
+-- et celles des personnes participant à cet évènement.
+SELECT ID_PERS
+FROM PARTICIPER
+WHERE ID_EVENT = 1;
+
+--   Pour une news, on veut savoir le nombre de commentaires qui ont été postés.
+SELECT COUNT(*)
+FROM COMMENTER
+WHERE ID_NEWS = 1;
