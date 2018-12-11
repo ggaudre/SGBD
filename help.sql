c64bbd4be3511d9cd27e19ef8590dd0a0b84a3e1
GAUDRE Gabin <ggaudre@capharnaum.pedago.ipb.fr>
2018-12-11 20:36:59 +0100

    ajout help

---
+++ help.sql
@@ -0,0 +1,19 @@
+PROMT "Requêtes de consultation : ";
+PROMT "Pour afficher :";
+PROMT "les lignes puis attributs d'une table de votre choix: @selection";
+PROMT "la liste des événements à venir: @future_events";
+PROMT "les informations sur les adhérents d'une association à une date donnée: @adherents ";
+PROMT "le bureau d'une association: @bureau";
+PROMT "la liste des commentaires à propos d'un événement: @com_event";
+PROMT "la liste des news d'une association: @news_asso";
+PROMT "la liste des dernières news: @news";
+PROMT "la liste des organisateurs d'un événement: @orga_event";
+PROMT "la liste des personnes y participant: @participer";
+PROMT "le nombre de commentaires postés pour une news: @com_news";
+PROMT "Requêtes statistiques:";
+PROMT "Pour afficher:";
+PROMT "la moyenne du nombre de commentaires donnés par chaque adhérent pour une news: @nb_com";
+PROMT "la moyenne des notes données par les participants à un événement: @avg_note_event";
+PROMT "le classement des événements en fonction de la note moyenne donnée par les participants à chacun de ces événements: @clas_event";
+PROMT "le classement des adhérents participant le plus aux événements: @clas_adherents"
+PROMT "le classement des financement d'une association à une date donnée en comptant les cotisations: @clas_financements"
