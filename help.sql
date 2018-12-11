5b234f6f8d17b1ef24b0e9d468e9f8474032d7f3
GAUDRE Gabin <ggaudre@capharnaum.pedago.ipb.fr>
2018-12-11 20:43:14 +0100

    ajout help

--- help.sql
+++ help.sql
@@ -1,19 +1,21 @@
-PROMPT "Requêtes de consultation : ";
-PROMPT "Pour afficher :";
-PROMPT "les lignes puis attributs d'une table de votre choix: @selection";
-PROMPT "la liste des événements à venir: @future_events";
-PROMPT "les informations sur les adhérents d'une association à une date donnée: @adherents ";
-PROMPT "le bureau d'une association: @bureau";
-PROMPT "la liste des commentaires à propos d'un événement: @com_event";
-PROMPT "la liste des news d'une association: @news_asso";
-PROMPT "la liste des dernières news: @news";
-PROMPT "la liste des organisateurs d'un événement: @orga_event";
-PROMPT "la liste des personnes y participant: @participer";
-PROMPT "le nombre de commentaires postés pour une news: @com_news";
-PROMPT "Requêtes statistiques:";
-PROMPT "Pour afficher:";
-PROMPT "la moyenne du nombre de commentaires donnés par chaque adhérent pour une news: @nb_com";
-PROMPT "la moyenne des notes données par les participants à un événement: @avg_note_event";
-PROMPT "le classement des événements en fonction de la note moyenne donnée par les participants à chacun de ces événements: @clas_event";
-PROMPT "le classement des adhérents participant le plus aux événements: @clas_adherents"
-PROMPT "le classement des financement d'une association à une date donnée en comptant les cotisations: @clas_financements"
+PROMPT
+PROMPT  Requêtes de consultation :  ;
+PROMPT  Pour afficher : ;
+PROMPT  les lignes puis attributs d'une table de votre choix: @selection ;
+PROMPT  la liste des événements à venir: @future_events ;
+PROMPT  les informations sur les adhérents d'une association à une date donnée: @adherents  ;
+PROMPT  le bureau d'une association: @bureau ;
+PROMPT  la liste des commentaires à propos d'un événement: @com_event ;
+PROMPT  la liste des news d'une association: @news_asso ;
+PROMPT  la liste des dernières news: @news ;
+PROMPT  la liste des organisateurs d'un événement: @orga_event ;
+PROMPT  la liste des personnes y participant: @participer ;
+PROMPT  le nombre de commentaires postés pour une news: @com_news ;
+PROMPT
+PROMPT  Requêtes statistiques: ;
+PROMPT  Pour afficher: ;
+PROMPT  la moyenne du nombre de commentaires donnés par chaque adhérent pour une news: @nb_com ;
+PROMPT  la moyenne des notes données par les participants à un événement: @avg_note_event ;
+PROMPT  le classement des événements en fonction de la note moyenne donnée par les participants à chacun de ces événements: @clas_event ;
+PROMPT  le classement des adhérents participant le plus aux événements: @clas_adherents
+PROMPT  le classement des financement d'une association à une date donnée en comptant les cotisations: @clas_financements
