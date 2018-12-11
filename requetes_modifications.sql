b175ab6c72f875a0f85d9a00f5af7a23b45fc492
Guerrat <salimi.mehdy@gmail.com>
2018-12-11 20:42:21 +0100

    a

--- requetes_modifications.sql
+++ requetes_modifications.sql
@@ -5,11 +5,7 @@ PROMPT "Ajout d'un adhérent"
 
 
 insert into PERSONNES values (ID_PERS_SEQ.nextval, 'DUBOIS', 'JEAN');
-VARIABLE id_personne NUMBER(5)
-begin
-exec SELECT ID_PERS into :id_personne from PERSONNES where NOM_PERS = 'DUBOIS'; -- Ici je sais qu'il n'y a qu'un seul Dubois
-end
-insert into ADHERENTS values ('jdubois', 'jdubois@enseirb-matmeca.fr', 'ELECTRONIQUE', 2021, 'mdp_jdubois', &id_personne);
+insert into ADHERENTS values ('jdubois', 'jdubois@enseirb-matmeca.fr', 'ELECTRONIQUE', 2021, 'mdp_jdubois', (select id_pers from personnes where NOM_PERS = 'DUBOIS'));
 
 -- Modification du mdp de l'adhérent
 PROMPT "Modification du mdp de l'adhérent"
@@ -18,4 +14,12 @@ update ADHERENTS set MDP = 'nouveau_mdp' where LOGIN_ADHERENT = 'jdubois';
 -- Suppression de l'adhérent
 PROMPT "Suppression de l'adhérent"
 delete from ADHERENTS where LOGIN_ADHERENT = 'jdubois';
-delete from PERSONNES where ID_PERS = &id_personne;
+
+
+-- Ajout d'un événement
+PROMT "Ajout d'un événement"
+insert into EVENEMENTS values (ID_EVENT_SEQ.nextval, "Event", "Ceci est l'évenement ajouté", '09-NOV-2018', 4000, 'ENSEIRB-MATMECA', 3, 5, 550, '14-NOV-2018', '14-NOV-2018', NULL);
+
+
+-- Ajout d'une association
+PROMT "Ajout d'une association"
