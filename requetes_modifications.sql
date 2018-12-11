01a90f2be4f07617801b357c41f520a9216e1d79
Guerrat <salimi.mehdy@gmail.com>
2018-12-11 18:21:05 +0100

    a

--- requetes_modifications.sql
+++ requetes_modifications.sql
@@ -1,8 +1,9 @@
 -- Ajout d'un adhérent
 PROMPT "Ajout d'un adhérent"
 define id_personne = ID_PERS_SEQ.nextval
-insert into PERSONNES values (&id_personne, 'DUBOIS', 'JEAN');
-insert into ADHERENTS values ('jdubois', 'jdubois@enseirb-matmeca.fr', 'ELECTRONIQUE', 2021, 'mdp_jdubois', &id_personne);
+define id_perso = id_personne
+insert into PERSONNES values (&id_perso, 'DUBOIS', 'JEAN');
+insert into ADHERENTS values ('jdubois', 'jdubois@enseirb-matmeca.fr', 'ELECTRONIQUE', 2021, 'mdp_jdubois', &id_perso);
 
 -- Modification du mdp de l'adhérent
 PROMPT "Modification du mdp de l'adhérent"
@@ -11,4 +12,4 @@ update ADHERENTS set MDP = 'nouveau_mdp' where LOGIN_ADHERENT = 'jdubois';
 -- Suppression de l'adhérent
 PROMPT "Suppression de l'adhérent"
 delete from ADHERENTS where LOGIN_ADHERENT = 'jdubois';
-delete from PERSONNES where ID_PERS = &id_personne;
+delete from PERSONNES where ID_PERS = &id_perso;
