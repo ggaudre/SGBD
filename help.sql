c9d83c9c81aed8a8305cb1a30338f9c9d55ac1c6
GAUDRE Gabin <ggaudre@capharnaum.pedago.ipb.fr>
2018-12-11 20:51:27 +0100

    ajout help

--- help.sql
+++ help.sql
@@ -17,5 +17,12 @@ PROMPT  Pour afficher: ;
 PROMPT  la moyenne du nombre de commentaires donnés par chaque adhérent pour une news: @nb_com ;
 PROMPT  la moyenne des notes données par les participants à un événement: @avg_note_event ;
 PROMPT  le classement des événements en fonction de la note moyenne donnée par les participants à chacun de ces événements: @clas_event ;
-PROMPT  le classement des adhérents participant le plus aux événements: @clas_adherents
-PROMPT  le classement des financement d'une association à une date donnée en comptant les cotisations: @clas_financements
+PROMPT  le classement des adhérents participant le plus aux événements: @clas_adherents ;
+PROMPT  le classement des financement d'une association à une date donnée en comptant les cotisations: @clas_financements ;
+PROMPT
+PROMPT Requêtes de modification: ;
+PROMPT Ajout d'un adhérent: @add_adherent ;
+PROMPT Modification du mot de passe d'un adhérent: @mdp ;
+PROMPT Suppression d'un adhérent: @del_adherent ;
+PROMPT Ajout d'un événement: @add_event ;
+PROMPT Ajout d'une association: @add_asso ;
