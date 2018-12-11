0dfe6e717b0106c1ed0ddc2a081cc7443c335216
Guerrat <salimi.mehdy@gmail.com>
2018-12-11 18:29:28 +0100

    a

--- requetes_modifications.sql
+++ requetes_modifications.sql
@@ -1,9 +1,9 @@
 -- Ajout d'un adhérent
 PROMPT "Ajout d'un adhérent"
-define id_personne = ID_PERS_SEQ.nextval
-define id_perso = id_personne
-insert into PERSONNES values (&id_perso, 'DUBOIS', 'JEAN');
-insert into ADHERENTS values ('jdubois', 'jdubois@enseirb-matmeca.fr', 'ELECTRONIQUE', 2021, 'mdp_jdubois', &id_perso);
+
+insert into PERSONNES values (ID_PERS_SEQ.nextval, 'DUBOIS', 'JEAN');
+define id_personne = select ID_PERS from PERSONNES where NOM_PERS = 'DUBOIS' -- Ici je sais qu'il n'y a qu'un seul Dubois
+insert into ADHERENTS values ('jdubois', 'jdubois@enseirb-matmeca.fr', 'ELECTRONIQUE', 2021, 'mdp_jdubois', &id_personne);
 
 -- Modification du mdp de l'adhérent
 PROMPT "Modification du mdp de l'adhérent"
@@ -12,4 +12,4 @@ update ADHERENTS set MDP = 'nouveau_mdp' where LOGIN_ADHERENT = 'jdubois';
 -- Suppression de l'adhérent
 PROMPT "Suppression de l'adhérent"
 delete from ADHERENTS where LOGIN_ADHERENT = 'jdubois';
-delete from PERSONNES where ID_PERS = &id_perso;
+delete from PERSONNES where ID_PERS = &id_personne;
